81e8cb62bc989d7f6b6ce1550b5fff231a87a224
demonickatsu <dinogit99@outlook.com>
2024-09-26 19:35:49 -0400

    amazing theme overhaul
    but we still suck at css

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("search").oninput = function(){
-        var searchterm = document.getElementById("search").value.toLocaleLowerCase();
+        var searchterm = document.getElementById("search").value.toLocaleLowerCase()
         var list_items = document.getElementsByClassName("games")
         for (var i = 0; i < list_items.length; i++) {
             var li = list_items[i]
             li.style.display = ""
-            if (li.textContent.toLocaleLowerCase().search(searchterm) == -1) {
+            if (li.textContent.toLocaleLowerCase().search(searchterm) == -1){
                 li.style.display = "none"
             }
         }
     }
-})
+})  
